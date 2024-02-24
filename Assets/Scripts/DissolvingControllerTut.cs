bbe83e395963907c43f032b9b1dd1dd6322ed48d
Tan <thaiminhnhattan2808@gmail.com>
2024-02-24 09:34:42 +0700

    FIx VFX DIe Player and Name Player

--- Assets/Scripts/DissolvingControllerTut.cs
+++ Assets/Scripts/DissolvingControllerTut.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -12,6 +13,8 @@ public class DissolvingControllerTut : MonoBehaviour
     public float refreshRate = 0.025f;
     
     private Material[] _skinnedMaterials;
+
+    [SerializeField]private PhotonView _view;
 
     private void Start()
     {
@@ -39,13 +42,27 @@ public class DissolvingControllerTut : MonoBehaviour
             while (_skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
             {
                 counter += dissolveRate;
-                for (int i = 0;i  < _skinnedMaterials.Length; i++)
-                {
-                    _skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
-                }
+                _view.RPC(nameof(Dead), RpcTarget.AllBuffered, counter);
 
                 yield return new WaitForSeconds(refreshRate);
             }
+        }
+    }
+
+    [PunRPC]
+    private void Dead(float counter)
+    {
+        for (int i = 0;i  < _skinnedMaterials.Length; i++)
+        {
+            _skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+        }
+    }
+
+    public void Revival()
+    {
+        for (int i = 0; i < _skinnedMaterials.Length; i++)
+        {
+            _skinnedMaterials[i].SetFloat("_DissolveAmount", 0);
         }
     }
 }
